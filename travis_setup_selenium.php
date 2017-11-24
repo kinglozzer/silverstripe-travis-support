69eaa9d9733288b606d96f583b9ac8161d390056
Loz Calver <lozcalver@bigfork.co.uk>
2017-11-24 16:51:52 +0000

    Chromedriver

--- travis_setup_selenium.php
+++ travis_setup_selenium.php
@@ -31,8 +31,10 @@ if(!putenv("XVFBARGS=:99 -ac -screen 0 {$resolution}x16")) echo "ERROR: Could no
 run("sh -e /etc/init.d/xvfb start");
 if(!putenv("DISPLAY=:99")) echo "ERROR: Could not set display!\n";
 run("wget https://selenium-release.storage.googleapis.com/2.41/selenium-server-standalone-2.41.0.jar");
+run("wget https://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip");
+run("unzip chromedriver_linux64.zip");
 if(!file_exists('artifacts')) mkdir('artifacts');
-run("java -jar selenium-server-standalone-2.41.0.jar > artifacts/selenium.log 2>&1 &");
+run('java -jar selenium-server-standalone-2.41.0.jar -Dwebdriver.chrome.driver="chromedriver" > artifacts/selenium.log 2>&1 &');
 sleep(5);
 
 // Write templated behat configuration
